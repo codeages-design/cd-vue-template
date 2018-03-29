d8be01e4699b63f967d1b10bf46d667b91e90a9c
zhongjie <zhongjie@howzhi.com>
2018-03-29 17:29:02 +0800

    细节优化

--- template/src/containers/course/list/index.vue
+++ template/src/containers/course/list/index.vue
@@ -25,7 +25,7 @@
         label="课程状态"
         width="120">
         <template scope="scope">
-          {{ scope.row.status|courseStatus }}
+          {{ scope.row.status | courseStatus }}
         </template>
       </el-table-column>
       <el-table-column
@@ -43,7 +43,7 @@
         label="课程类型"
         width="120">
         <template scope="scope">
-          {{ scope.row.type|courseType }}
+          {{ scope.row.type | courseType }}
         </template>
       </el-table-column>
       <el-table-column
@@ -56,14 +56,14 @@
         label="创建时间"
         width="200">
         <template scope="scope">
-          {{ scope.row.createdTime|datetime }}
+          {{ scope.row.createdTime | datetime }}
         </template>
       </el-table-column>
       <el-table-column
         label="更新时间"
         width="200">
         <template scope="scope">
-          {{ scope.row.updatedTime|datetime }}
+          {{ scope.row.updatedTime | datetime }}
         </template>
       </el-table-column>
     </el-table>
