23b919a049eddb4e86527ea846e3db8d3cd08028
zhongjie <zhongjie@howzhi.com>
2018-03-29 16:17:41 +0800

    init

---
+++ meta.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "version": {
+      "type": "string",
+      "required": false,
+      "message": "Project version",
+      "default": '1.0.0'
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A Vue.js project"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    }
+  }
+}
