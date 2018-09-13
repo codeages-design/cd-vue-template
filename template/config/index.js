99a273d3efc4efc86dbc86f0337d320befd9ee60
fanxiaocheng <fanxiaocheng@howzhi.com>
2018-09-13 16:33:23 +0800

    调整路由替换机制

--- template/config/index.js
+++ template/config/index.js
@@ -9,6 +9,8 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    jsonrpcUrl: '/',
+    apiUrl: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -28,10 +30,21 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    jsonrpcUrl: '/jsonrpc',
+    apiUrl: '/api',
     proxyTable: {
       '/api': {
         target: 'http://t5.edusoho.cn',
         changeOrigin: true
+      },
+      '/jsonrpc': {
+        target: 'http://eslive.qiqiuyun.net',
+        // target: 'http://eslive.local.cg-dev.cn',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/jsonrpc': ''
+        },
+        secure: false
       }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
